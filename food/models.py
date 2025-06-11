f22678c37e2f07584e44d9c69a54a53764831444
Ahmed Shakib Reza <shakibreza003@gmail.com>
2025-06-11 22:19:45 +0600

    update

--- food/models.py
+++ food/models.py
@@ -5,6 +5,13 @@ from django.db import models
 # models are the database schema, which defines the structure of your data.
 
 class Item(models.Model):
-    # Now we define the fields of the model.
-    item_name = 
 
+    def __str__(self):
+        # This method defines how the model is represented as a string.
+        return self.item_name
+
+    # Now we define the "fields" of the model.
+    item_name = models.CharField(max_length=200)
+    item_desc = models.CharField(max_length=200)
+    item_price = models.IntegerField()
+ 
--- food/views.py
+++ food/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Item  # Import the Item model to use in views
+from django.template import loader
 # Create your views here.
 
 
-def index(request): # landing page of food app
+def index(request):  # landing page of food app
     # This function handles the request to the landing page of the food app
-    return HttpResponse("Hello World!")
+    item_list = Item.objects.all()  # Fetch all items from the database
+    
+    # Load the template for the index page
+    # template = loader.get_template('food/index.html')
+    
+    # Render the template with the item list context
+    # template always needs to be rendered with a context
+    # context is the data that will be passed to the template from database
+    context = {
+        'item_list': item_list,  # Pass the item list to the template
+    }
+
+    return render(request, "food/index.html", context)
+    
+    # Return the rendered template as an HttpResponse
+    # return HttpResponse(template.render(context, request))
+
 
 # food/item/ will be handled by this function
-def item(request): 
+def item(request):
     return HttpResponse("This is an item view")
