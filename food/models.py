91d658de5cdfcc976841cd18ca8553fe9fccee8c
Ahmed Shakib Reza <shakibreza003@gmail.com>
2025-06-12 17:18:21 +0600

    update

--- food/models.py
+++ food/models.py
@@ -14,4 +14,5 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://cdn.vectorstock.com/i/500p/42/11/creative-concept-of-brain-food-symbolized-vector-53434211.jpg")
  
